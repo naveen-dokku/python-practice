467556e9cabf30562dd409c774d55cddab6fd812
naveen dokku <naveendokku@gmail.com>
2020-06-12 22:27:12 -0400

    Python intial commit

---
+++ src/main/java/com/pythonpractice/PythonPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.pythonpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PythonPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PythonPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pythonpractice/PythonPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pythonpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PythonPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
